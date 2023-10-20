3ec31e3ebe9b3e6b5179b108796222ddd0021d9a
amit sharma <141321471+7526943851amit@users.noreply.github.com>
2023-10-20 17:25:41 +0530

    border spspiiner

--- header.php
+++ header.php
@@ -1,86 +1,173 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <meta charset="utf-8" />
-      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <title>Home</title>
-      <meta content="" name="description" />
-      <meta content="" name="keywords" />
-      <!-- Favicons -->
-      <link href="" rel="icon" />
-      <link href="" rel="apple-touch-icon" />
-      <!-- Vendor CSS Files -->
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" />
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
-      <!-- Template Main CSS File -->
-      <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/style.css" rel="stylesheet" />
-      <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.png" type="image/x-icon">
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Fashion</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+      <!-- font awesome -->
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!--  -->
+      <!-- owl stylesheets -->
+      <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Poppins:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/owl.carousel.min.css">
+      <link rel="stylesoeet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <div id="loader-container">
+      <style>
+.page-numbers{
+    display: none;
+}
+.woocommerce-pagination {
+    display: none;
+}
+</style>
    </head>
    <body>
-      <!-- ======= Header ======= -->
-      <header id="header">
-         <!-- =======Top Header ======= -->
-         <div class="top-header">
+
+   <div id="loader">
+    <img src="http://localhost/woocomerce_project/wordpress/wp-content/uploads/2023/10/amalie-steiness.gif" alt="Loading...">
+  </div>
+</div>
+
+
+
+
+      <!-- banner bg main start -->
+      <div class="banner_bg_main">
+         <!-- header top section start -->
+         <div class="container">
+            <div class="header_section_top">
+               <div class="row">
+                  <div class="col-sm-12">
+                     <div class="custom_menu">
+                        <ul>
+                           <li><a href="#">Best Sellers</a></li>
+                           <li><a href="#">Gift Ideas</a></li>
+                           <li><a href="#">New Releases</a></li>
+                           <li><a href="#">Today's Deals</a></li>
+                           <li><a href="#">Customer Service</a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- header top section start -->
+         <!-- logo section start -->
+         <div class="logo_section">
             <div class="container">
-               <div class="row d-flex align-items-center">
-                  <div class="col-lg-6 col-md-6 col-sm-6">
-                     <div class="mail">
-                        <a href="mailto:hello@buymysku.com">hello@buymysku.com</a>
+               <div class="row">
+                  <div class="col-sm-12">
+                     <div class="logo"><a href="index.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.png"></a></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- logo section end -->
+         <!-- header section start -->
+         <div class="header_section">
+            <div class="container">
+               <div class="containt_main">
+                  <div id="mySidenav" class="sidenav">
+                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                     <a href="index.html">Home</a>
+                     <a href="fashion.html">Fashion</a>
+                     <a href="electronic.html">Electronic</a>
+                     <a href="jewellery.html">Jewellery</a>
+                  </div>
+                  <span class="toggle_icon" onclick="openNav()"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/toggle-icon.png"></span>
+                  <div class="dropdown">
+                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Category 
+                     </button>
+                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
                      </div>
                   </div>
-                  <div class="col-lg-6 col-md-6 col-sm-6 log-info">
-                     <div class="account">
-                        <a href="#">
-                           <div class="user-img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/user.png"></div>
-                           <div class="log-account">
-                              <p>My Account</p>
-                           </div>
+                  <div class="main">
+                     <!-- Another variation with a button -->
+                     <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search this blog">
+                        <div class="input-group-append">
+                           <button class="btn btn-secondary" type="button" style="background-color: #f26522; border-color:#f26522 ">
+                           <i class="fa fa-search"></i>
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="header_box">
+                     <div class="lang_box ">
+                        <a href="#" title="Language" class="nav-link" data-toggle="dropdown" aria-expanded="true">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/flag-uk.png" alt="flag" class="mr-2 " title="United Kingdom"> English <i class="fa fa-angle-down ml-2" aria-hidden="true"></i>
                         </a>
+                        <div class="dropdown-menu ">
+                           <a href="#" class="dropdown-item">
+                           <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/flag-france.png" class="mr-2" alt="flag">
+                           French
+                           </a>
+                        </div>
                      </div>
-                     <div class="login">
-                        <a href="#" class="lbtn">Login</a>
+                     <div class="login_menu">
+                        <ul>
+                           <li><a href="#">
+                              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                              <span class="padding_10">Cart</span></a>
+                           </li>
+                           <li><a href="#">
+                              <i class="fa fa-user" aria-hidden="true"></i>
+                              <span class="padding_10">Cart</span></a>
+                           </li>
+                        </ul>
                      </div>
                   </div>
                </div>
             </div>
          </div>
-         <div class="container d-flex align-items-center">
-            <a href="index.html" class="logo me-auto"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.png" alt="" class="img-fluid" /></a>
-            <nav id="navbar" class="navbar order-last order-lg-0">
-               <ul>
-                  <li><a class="nav-link scrollto" href="#hero">About</a></li>
-                  <li><a class="nav-link scrollto" href="#about">Buy my SQU</a></li>
-                  <li><a class="nav-link scrollto" href="#services">Sell my SKU</a></li>
-                  <li><a class="nav-link scrollto" href="#blog">Pricing</a></li>
-                  <li><a class="nav-link scrollto" href="#portfolio">Blog</a></li>
-                  <!--  <li class="dropdown">
-                     <a href="#"><span>Career</span> <i class="bi bi-chevron-down"></i></a>
-                     <ul>
-                         <li><a href="#">Drop Down 1</a></li>
-                         <li class="dropdown">
-                             <a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                             <ul>
-                                 <li><a href="#">Deep Drop Down 1</a></li>
-                                 <li><a href="#">Deep Drop Down 2</a></li>
-                                 <li><a href="#">Deep Drop Down 3</a></li>
-                                 <li><a href="#">Deep Drop Down 4</a></li>
-                                 <li><a href="#">Deep Drop Down 5</a></li>
-                             </ul>
-                         </li>
-                     </ul>
-                     </li> -->
-                  <li><a class="nav-link scrollto" href="#contact">FAQs</a></li>
-               </ul>
-              <!--  <i class="fa-solid fa-bars-staggered mobile-nav-toggle"></i> -->
-               <i class="fa-solid fa-bars mobile-nav-toggle"></i>
-            </nav>
-            <div class="input-group">
-               <div class="form-outline">
-                  <input type="search" id="form1" class="form-control" />
-                  <i class="fas fa-search"></i>
-               </div>
-            </div>
-         </div>
-      </header>
+
+		<?php wp_head(); ?>
+
+      <div id="loader-container">
+
+</div>
+<script>
+jQuery(document).ready(function($) {
+    var loader = $('#loader');
+    
+    
+    $(window).on('beforeunload', function() {
+        loader.show();
+    });
+
+   
+    $(window).on('load', function() {
+        loader.hide();
+    });
+});
+</script>
+
