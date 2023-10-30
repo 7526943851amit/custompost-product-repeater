f34d6dbdd18be0480d741e511fe4a34b99292366
amit sharma <141321471+7526943851amit@users.noreply.github.com>
2023-10-30 12:36:50 +0530

    Create emaildesign.php

---
+++ emaildesign.php
@@ -0,0 +1,17 @@
+Hello Admin,
+
+A contact form has been submitted on [_site_title]. 
+
+Following are it's details:
+
+Name: [your-name]
+Email:[your-email]
+Subject :[your-subject]
+textarea :[your-message]
+-- 
+This is a notification that a contact form was submitted on your website ([_site_title] [_site_url]).
+
+
+
+Regards,
+[_site_title]
