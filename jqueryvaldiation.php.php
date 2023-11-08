fd03ed6b34932ea73db115e3d5bc9707b3358104
amit sharma <141321471+7526943851amit@users.noreply.github.com>
2023-11-08 11:59:27 +0530

    Add files via upload

---
+++ jqueryvaldiation.php.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $('#btn').click(function(e){
+	 e.preventDefault();  
+	var name = $('#fname').val();
+	var lname = $('#lname').val();
+	var email = $('#email').val();
+	var age = $('#age').val();
+	if(name === ""){
+		$('#fname_error').text('First name is required');
+	}
+	else if((name.length<=4) && (name.length >=10)){
+		$('#fname_error').text('First name is toos hot');
+	}
+	else {
+		$('#fname_error').text('');
+	}
+
+	if(lname === ""){
+		$('#lname_error').text('Last name is required');
+	}
+	else {
+		$('#lname_error').text('');
+	}
+
+	if(email === ""){
+		$('#email_error').text('Email is required');
+	}
+	else {
+		$('#email_error').text('');
+	}
+
+	if(age === ""){
+		$('#age_error').text('Age is required');
+	}
+	else {
+		$('#age_error').text('');
+	}
+  });
+});
+</script>
+</head>
+<body>
+
+<p>If you click on me, I will disappear.</p>
+<input type="text"  id="fname" /><span id="fname_error"></span>
+<input type="text"  id="lname" /><span id="lname_error"></span>
+<input type="text"  id="email" /><span id="email_error"></span>
+<input type="text"  id="age" /><span id="age_error"></span>
+<button id="btn">My button</button>
+
+</body>
+</html>
